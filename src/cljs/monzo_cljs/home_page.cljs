06b30794dbfdf9c76192b85dd44e8aea9f7e37c7
David Martin <dwmartin41@gmail.com>
2017-01-25 23:42:21 +0000

    moved transactions card into its own component, also increases performance

--- src/cljs/monzo_cljs/home_page.cljs
+++ src/cljs/monzo_cljs/home_page.cljs
@@ -43,6 +43,75 @@
     (when (= 1 (count currencies))
       (first currencies))))
 
+(defn transactions-card-header [event-chan loading?]
+  [CardTitle {:class "home-card__title"}
+   [:h2 {:class "mdl-card__title-text"} "Transactions"]
+   (if loading?
+     [Spinner]
+     [IconButton {:class "home-card__refresh"
+                  :name "refresh"
+                  :on-click #(go (put! event-chan [:action/refresh-transactions]))}])])
+
+(defn transactions-card-body [data]
+  [:div {:class "mdl-card__supporting-text"}
+   (->> data
+        (group-by (comp (juxt year month day)
+                        #(goog.date.DateTime. %)
+                        second
+                        :transaction))
+        (sort-by first)
+        reverse
+        (map (fn [[date-parts day-data]]
+               (let [day-transactions (map :transaction day-data)]
+                 ^{:key date-parts}
+                 [:div {:class "group"}
+                  [:h4 {:class "group__header"} (format-date (apply time/date-time date-parts))]
+                  (let [sum (sum-transactions day-transactions)
+                        currency (get-transactions-currency day-transactions)]
+                    (when currency
+                      [:h5 {:class (str "group__sub-header "
+                                        (if (pos? sum)
+                                          "group__sub-header--positive"
+                                          "group__sub-header--negative"))}
+                       (str " " (format-amount currency sum))]))
+                  [List
+                   (->> day-data
+                        (sort-by (comp second :transaction) <)
+                        (map (fn [{[id created amount desc currency {notes :notes} decline-reason included? :as transaction] :transaction
+                                   [icon logo merchant address] :merchant}]
+                               (let [is-credit (pos? amount)
+                                     declined (declined? transaction)]
+                                 ^{:key id}
+                                 [ListItem {:class (str "transaction "
+                                                        (if is-credit "transaction--credit" "transaction--debit"))}
+                                  (if (not (blank? logo))
+                                    [:img {:class "mdl-list__item-icon transaction__icon"
+                                           :src logo}]
+                                    [:span {:class "mdl-list__item-icon transaction__icon"}
+                                     (or icon "💰")])
+                                  [:span {:class "mdl-list__item-primary-content transaction__text"}
+                                   [:span {:class (str "transaction__amount "
+                                                       (when declined
+                                                         "transaction__amount--not-included"))}
+                                    (format-amount currency (js/Math.abs amount))]
+                                   [:span {:class "transaction__description-lines"}
+                                    [:span {:class "transaction__description-primary"}
+                                     (or merchant desc)]
+                                    (let [addr (:short_formatted address)]
+                                      [:span {:class  (str "transaction__description-secondary "
+                                                           (when declined
+                                                             "transaction__description-secondary--warning"))}
+                                       (or (get decline-reasons decline-reason)
+                                           notes
+                                           addr)])]
+                                   [:span {:class "transaction__date"}
+                                    (format-time (goog.date.DateTime. created))]]]))))]]))))])
+
+(defn transactions-card [event-chan loading? data]
+  [Card {:class "home-card"}
+   [transactions-card-header event-chan loading?]
+   [transactions-card-body data]])
+
 (defn home-page [app-db event-chan]
   (let [data (->> (q '[:find ?e ?created ?amount ?desc ?currency ?metadata ?decline-reason ?include ?m
                        :in $
@@ -63,69 +132,9 @@
                                         (-> (d/pull app-db '[:merchant/emoji :merchant/logo :merchant/name :merchant/address]
                                                    m-id)
                                             ((juxt :merchant/emoji :merchant/logo :merchant/name :merchant/address))))}))))
-        {loading :transactions/loading} (pull app-db '[:transactions/loading] app-datom-id)]
+        {loading? :transactions/loading} (pull app-db '[:transactions/loading] app-datom-id)]
     (if (empty? data)
       
       [Spinner]
       
-      [Card {:class "home-card"}
-       [CardTitle {:class "home-card__title"}
-        [:h2 {:class "mdl-card__title-text"} "Transactions"]
-        (if loading
-          [Spinner]
-          [IconButton {:class "home-card__refresh"
-                       :name "refresh"
-                       :on-click #(go (put! event-chan [:action/refresh-transactions]))}])]
-       [:div {:class "mdl-card__supporting-text"}
-        (->> data
-             (group-by (comp (juxt year month day)
-                             #(goog.date.DateTime. %)
-                             second
-                             :transaction))
-             (sort-by first)
-             reverse
-             (map (fn [[date-parts day-data]]
-                    (let [day-transactions (map :transaction day-data)]
-                      ^{:key date-parts}
-                      [:div {:class "group"}
-                       [:h4 {:class "group__header"} (format-date (apply time/date-time date-parts))]
-                       (let [sum (sum-transactions day-transactions)
-                             currency (get-transactions-currency day-transactions)]
-                         (when currency
-                           [:h5 {:class (str "group__sub-header "
-                                             (if (pos? sum)
-                                               "group__sub-header--positive"
-                                               "group__sub-header--negative"))}
-                            (str " " (format-amount currency sum))]))
-                       [List
-                        (->> day-data
-                             (sort-by (comp second :transaction) <)
-                             (map (fn [{[id created amount desc currency {notes :notes} decline-reason included? :as transaction] :transaction
-                                        [icon logo merchant address] :merchant}]
-                                    (let [is-credit (pos? amount)
-                                          declined (declined? transaction)]
-                                      ^{:key id}
-                                      [ListItem {:class (str "transaction "
-                                                             (if is-credit "transaction--credit" "transaction--debit"))}
-                                       (if (not (blank? logo))
-                                         [:img {:class "mdl-list__item-icon transaction__icon"
-                                                :src logo}]
-                                         [:span {:class "mdl-list__item-icon transaction__icon"}
-                                          (or icon "💰")])
-                                       [:span {:class "mdl-list__item-primary-content transaction__text"}
-                                        [:span {:class (str "transaction__amount "
-                                                            (when declined
-                                                              "transaction__amount--not-included"))}
-                                         (format-amount currency (js/Math.abs amount))]
-                                        [:span {:class "transaction__description-lines"}
-                                         [:span {:class "transaction__description-primary"}
-                                          (or merchant desc)]
-                                         (let [addr (:short_formatted address)]
-                                           [:span {:class  (str "transaction__description-secondary "
-                                                             (when declined
-                                                               "transaction__description-secondary--warning"))}
-                                            (or (get decline-reasons decline-reason)
-                                                notes
-                                                addr)])]
-                                        [:span {:class "transaction__date"}
-                                         (format-time (goog.date.DateTime. created))]]]))))]]))))]])))
+      [transactions-card event-chan loading? data])))
