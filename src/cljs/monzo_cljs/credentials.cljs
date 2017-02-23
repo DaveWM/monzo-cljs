68e218818bf7c1a0041dc7ccd446ae44d905116e
David Martin <dwmartin41@gmail.com>
2017-02-23 21:41:19 +0000

    removed credentials file (with old credentials) and added to .gitignore

--- src/cljs/monzo_cljs/credentials.cljs
+++
@@ -1,4 +0,0 @@
-(ns monzo-cljs.credentials)
-
-(def credentials {:client-id "oauthclient_00009E3Bi8qsbFvOjJVpXV"
-                  :client-secret "8V+rwPqM5+024EEk+88lj6IwG/wkX6aqMWI/Jefd+jQxWhV5sH0htkH22CYAeRRYApl6ezceeitKX+cm3dAu"})
